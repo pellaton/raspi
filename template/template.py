0de02923b7989536bc06cddaa9b935678d631439
root <root@asahi.(none)>
2014-12-20 22:10:58 +0100

    add template

---
+++ template/template.py
@@ -0,0 +1,14 @@
+import RPi.GPIO as GPIO 
+import time
+
+HIGH = GPIO.HIGH
+LOW = GPIO.LOW
+
+GPIO.setmode(GPIO.BCM)
+
+def __Main():
+  
+try:
+  __Main()
+except KeyboardInterrupt:
+  GPIO.cleanup()
