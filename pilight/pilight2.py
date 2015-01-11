4ea512bc3e7f60b8ea5857027ecb8d2c48c8c0d5
pitschna <pitschna@gmail.com>
2015-01-11 19:37:52 +0100

    Use Google style guide for python

--- pilight/pilight2.py
+++ pilight/pilight2.py
@@ -30,42 +30,42 @@ GPIO.setup(LAMP_ON, GPIO.OUT)
 GPIO.setup(LAMP_OFF, GPIO.OUT) 
 
 # 'a': append to existing file
-logFile = open('/var/log/pilight2.log', 'a')
+log_file = open('/var/log/pilight2.log', 'a')
 
-def __ActuateSwitch(pin):
+def actuate_switch(pin):
   GPIO.output(pin, HIGH)
   time.sleep(0.2)
   GPIO.output(pin, LOW)
 
-def __SwitchLampOn():
-  __ActuateSwitch(LAMP_ON)
+def switch_lamp_on():
+  actuate_switch(LAMP_ON)
 
-def __SwitchLampOff():
-  __ActuateSwitch(LAMP_OFF)
+def switch_lamp_off():
+  actuate_switch(LAMP_OFF)
 
-def __SendClock():
+def send_clock():
   GPIO.output(SCLK, HIGH)
   GPIO.output(SCLK, LOW)
 
-def __ReadValue(channel):
+def read_value(channel):
   GPIO.output(CS, HIGH)
   # enable chip
   GPIO.output(CS, LOW)
 
   # send command : start start = 1, single=1, channel=000 
-  sendcmd = 0b00011000 | channel
-  for commandBits in range(5):
-    if (sendcmd & 0x10):
+  send_cmd = 0b00011000 | channel
+  for command_bits in range(5):
+    if (send_cmd & 0x10):
       GPIO.output(MOSI, HIGH)
     else:
       GPIO.output(MOSI, LOW)
-    __SendClock()
-    sendcmd <<= 1
+    send_clock()
+    send_cmd <<= 1
       
   # readout
   value = 0
-  for dataBits in range(13):
-    __SendClock()
+  for data_bits in range(13):
+    send_clock()
     value <<= 1
     if (GPIO.input(MISO)):
       value |= 0x01
@@ -75,54 +75,54 @@ def __ReadValue(channel):
 
   return value
  
-def __MeasureBrightness():
-  sumValue = [0.0, 0.0]
-  samplePoints = 10
+def measure_brightness():
+  sum_value = [0.0, 0.0]
+  sample_points = 10
  
   GPIO.output(PWR, HIGH)
-  for i in range(samplePoints):
-    value0 = __ReadValue(0)
-    value1 = __ReadValue(1)
-    sumValue[0] += value0;
-    sumValue[1] += value1;
+  for i in range(sample_points):
+    value_0 = read_value(0)
+    value_1 = read_value(1)
+    sum_value[0] += value_0
+    sum_value[1] += value_1
 
   GPIO.output(PWR, LOW)
 
-  return [sumValue[0]/samplePoints, sumValue[1]/samplePoints] 
+  return [sum_value[0]/sample_points, sum_value[1]/sample_points] 
 
-def __WriteLog(dateTime, brightnessOut, brightnessIn, lampState):
-  print(dateTime.isoformat(), '{:6.1f}'.format(brightnessOut),
-  '{:6.1f}'.format(brightnessIn), lampState,  file=logFile) 
-  logFile.flush()
+def write_log(date_time, brightness_out, brightness_in, lamp_state):
+  print(date_time.isoformat(), '{:6.1f}'.format(brightness_out),
+  '{:6.1f}'.format(brightness_in), lamp_state,  file=log_file) 
+  log_file.flush()
 
-def __cleanup():
+def cleanup():
   GPIO.cleanup()
-  print('#', datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S"), 'PiLight stopped', file=logFile) 
-  logFile.close()
+  print('#', datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S"), 'PiLight stopped', file=log_file) 
+  log_file.close()
   sys.exit(0)
 
-def __Main():
-  print('#',datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S"), 'PiLight started', file=logFile) 
+def main():
+  print('#',datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S"), 'PiLight started', file=log_file) 
   while(True):
     now = datetime.datetime.now()
-    brightnessOut, brightnessIn = __MeasureBrightness()
+    brightness_out, brightness_in = measure_brightness()
     if (now.hour >= 7 and now.hour < 21 ):
-      if (brightnessOut <= 3450):
-        __WriteLog(now, brightnessOut, brightnessIn, 'on')
-        __SwitchLampOn()
-      elif (brightnessOut >= 3750):
-        __WriteLog(now, brightnessOut, brightnessIn, 'off')
-        __SwitchLampOff()
+      if (brightness_out <= 3450):
+        write_log(now, brightness_out, brightness_in, 'on')
+        switch_lamp_on()
+      elif (brightness_out >= 3750):
+        write_log(now, brightness_out, brightness_in, 'off')
+        switch_lamp_off()
       else:
-        __WriteLog(now, brightnessOut, brightnessIn, 'n/a')
+        write_log(now, brightness_out, brightness_in, 'n/a')
     else:
-      __SwitchLampOff()
-      __WriteLog(now, brightnessOut, brightnessIn, 'off')
+      switch_lamp_off()
+      write_log(now, brightness_out, brightness_in, 'off')
     time.sleep(10*60)
 
 try:
-  signal.signal(signal.SIGINT, __cleanup)
-  signal.signal(signal.SIGTERM, __cleanup)
-  __Main()
+  signal.signal(signal.SIGINT, cleanup)
+  signal.signal(signal.SIGTERM, cleanup)
+  main()
 finally:
-  __cleanup()
+  cleanup()
