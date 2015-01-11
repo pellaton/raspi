7152b3724015d3d29dc35c40be6ebf1f1817af43
pitschna <pitschna@gmail.com>
2015-01-11 10:32:04 +0100

    Improved template by using signal handling with finally

--- template/template.py
+++ template/template.py
@@ -1,15 +1,20 @@
 import RPi.GPIO as GPIO 
 import time
+import signal
 
 HIGH = GPIO.HIGH
 LOW = GPIO.LOW
 
 GPIO.setmode(GPIO.BCM)
+def __cleanup():
+  GPIO.cleanup()
+  sys.exit(0)
 
 def __Main():
   
 try:
+  signal.signal(signal.SIGINT, __cleanup)
+  signal.signal(signal.SIGTERM, __cleanup)
   __Main()
-  GPIO.cleanup()
-except KeyboardInterrupt:
-  GPIO.cleanup()
+finally:
+  __cleanup()
